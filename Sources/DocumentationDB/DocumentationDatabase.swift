bf836198e9bccb324cabe4efe74410567c1f69f9
Ambrus Toth <ping@ambrus.dev>
2024-05-05 16:25:58 +0000

    Introduced very basic symbol types, and all asset types.

--- Sources/DocumentationDB/DocumentationDatabase.swift
+++ Sources/DocumentationDB/DocumentationDatabase.swift
@@ -1,3 +1,20 @@
+import Foundation
+
+/// A project URL is a URL that points to a file or folder within the project.
+///
+/// We use a custom scheme `project://` to distinguish these URLs from regular URLs.
+public struct ProjectURL {
+    public let url: URL
+
+    public init(_ url: URL) {
+        assert(url.isFileURL && url.scheme == "project", "URL must be a file url with the scheme project://") 
+        self.url = url
+    }
+}
+
+
 public struct DocumentationDatabase {
-    // todo
+    var rootModuleId: ModuleAsset.ID
+    var assets: AssetDatabase = .init()
+    var symbols: SymbolDatabase = .init()
 }
