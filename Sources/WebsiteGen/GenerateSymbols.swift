6cdfde1d759d0a7b6f61229c8d58a285730c382a
Thomas van Weert <t.c.o.vanweert@student.tudelft.nl>
2024-05-15 17:15:07 +0200

    ADD: traverse symbols

---
+++ Sources/WebsiteGen/GenerateSymbols.swift
@@ -0,0 +1,133 @@
+import FrontEnd
+import DocumentationDB
+
+/// Generate associated-type page
+///
+/// - Parameters:
+///   - ctx: context for page generation
+///   - of: associated-type declaration
+///   - with: associated-type documentation
+public func GenerateAssociatedTypePage(ctx: GenerationContext, of: AssociatedTypeDecl, with: AssociatedTypeDocumentation) {
+    
+}
+
+/// Generate associated-value page
+///
+/// - Parameters:
+///   - ctx: context for page generation
+///   - of: associated-value declaration
+///   - with: associated-value documentation
+public func GenerateAssociatedValuePage(ctx: GenerationContext, of: AssociatedValueDecl, with: AssociatedValueDocumentation) {
+    
+}
+
+/// Generate type-alias page
+///
+/// - Parameters:
+///   - ctx: context for page generation
+///   - of: type-alias declaration
+///   - with: type-alias documentation
+public func GenerateTypeAliasPage(ctx: GenerationContext, of: TypeAliasDecl, with: TypeAliasDocumentation) {
+    
+}
+
+/// Generate binding page
+///
+/// - Parameters:
+///   - ctx: context for page generation
+///   - of: binding declaration
+///   - with: binding documentation
+public func GenerateBindingPage(ctx: GenerationContext, of: BindingDecl, with: BindingDocumentation) {
+    
+}
+
+/// Generate operator page
+///
+/// - Parameters:
+///   - ctx: context for page generation
+///   - of: operator declaration
+///   - with: operator documentation
+public func GenerateOperatorPage(ctx: GenerationContext, of: OperatorDecl, with: OperatorDocumentation) {
+    
+}
+
+/// Generate function page
+///
+/// - Parameters:
+///   - ctx: context for page generation
+///   - of: function declaration
+///   - with: function documentation
+public func GenerateFunctionPage(ctx: GenerationContext, of: FunctionDecl, with: FunctionDocumentation) {
+    
+}
+
+/// Generate method page
+///
+/// - Parameters:
+///   - ctx: context for page generation
+///   - of: method declaration
+///   - with: method documentation
+public func GenerateMethodPage(ctx: GenerationContext, of: MethodDecl, with: MethodDeclDocumentation) {
+    
+}
+
+/// Generate method-implementation page
+///
+/// - Parameters:
+///   - ctx: context for page generation
+///   - of: method-implementation declaration
+///   - with: method-implementation documentation
+public func GenerateMethodImplementationPage(ctx: GenerationContext, of: MethodImpl, with: MethodImplDocumentation) {
+    
+}
+
+/// Generate subscript page
+///
+/// - Parameters:
+///   - ctx: context for page generation
+///   - of: subscript declaration
+///   - with: subscript documentation
+public func GenerateSubscriptPage(ctx: GenerationContext, of: SubscriptDecl, with: SubscriptDeclDocumentation) {
+    
+}
+
+/// Generate subscript-implementation page
+///
+/// - Parameters:
+///   - ctx: context for page generation
+///   - of: subscript-implementation declaration
+///   - with: subscript-implementation documentation
+public func GenerateSubscriptImplementationPage(ctx: GenerationContext, of: SubscriptImpl, with: SubscriptImplDoc) {
+    
+}
+
+/// Generate initializer page
+///
+/// - Parameters:
+///   - ctx: context for page generation
+///   - of: initializer declaration
+///   - with: initializer documentation
+public func GenerateInitializerPage(ctx: GenerationContext, of: InitializerDecl, with: InitializerDocumentation) {
+    
+}
+
+/// Generate trait page
+///
+/// - Parameters:
+///   - ctx: context for page generation
+///   - of: trait declaration
+///   - with: trait documentation
+public func GenerateTraitPage(ctx: GenerationContext, of: TraitDecl, with: TraitDocumentation) {
+
+}
+
+
+/// Generate product-type page
+///
+/// - Parameters:
+///   - ctx: context for page generation
+///   - of: product-type declaration
+///   - with: product-type documentation
+public func GenerateProductTypePage(ctx: GenerationContext, of: ProductTypeDecl, with: ProductTypeDocumentation) {
+
+}
--- Sources/WebsiteGen/Traversal.swift
+++ Sources/WebsiteGen/Traversal.swift
@@ -2,6 +2,7 @@ import Foundation
 import FrontEnd
 import DocumentationDB
 import Stencil
+import DequeModule
 
 /// Generate a module and its child assets
 ///
@@ -50,6 +51,136 @@ public func TraverseModuleChildren(ctx: GenerationContext, of: [AnyAssetID]) {
 /// - Parameters:
 ///   - ctx: context used for page generation
 ///   - rootNode: node to start traversing from
-public func TraverseSymbols(_ ctx: GenerationContext, _ rootNode: TranslationUnit) {
-    //TODO Traverse node -> node.decls
+public func TraverseSymbols(ctx: GenerationContext, rootNode: TranslationUnit.ID) {
+    var stack: Deque<AnyDeclID> = Deque(ctx.typedProgram.ast[rootNode].decls)
+    while let cursor = stack.popLast() {
+        switch cursor.kind {
+        case AssociatedTypeDecl.self:
+            let id = AssociatedTypeDecl.ID(cursor)!
+            let decl = ctx.typedProgram.ast[id]!
+            
+            // Generate page
+            let declDoc = ctx.documentation.symbols.associatedTypeDocs[astNodeId: id]!
+            GenerateAssociatedTypePage(ctx: ctx, of: decl, with: declDoc)
+            break
+        case AssociatedValueDecl.self:
+            let id = AssociatedValueDecl.ID(cursor)!
+            let decl = ctx.typedProgram.ast[id]!
+            
+            // Generate page
+            let declDoc = ctx.documentation.symbols.associatedValueDocs[astNodeId: id]!
+            GenerateAssociatedValuePage(ctx: ctx, of: decl, with: declDoc)
+            break
+        case TypeAliasDecl.self:
+            let id = TypeAliasDecl.ID(cursor)!
+            let decl = ctx.typedProgram.ast[id]!
+            
+            // Generate page
+            let declDoc = ctx.documentation.symbols.TypeAliasDocs[astNodeId: id]!
+            GenerateTypeAliasPage(ctx: ctx, of: decl, with: declDoc)
+            break
+        case BindingDecl.self:
+            let id = BindingDecl.ID(cursor)!
+            let decl = ctx.typedProgram.ast[id]!
+            
+            // Generate page
+            let declDoc = ctx.documentation.symbols.BindingDocs[astNodeId: id]!
+            GenerateBindingPage(ctx: ctx, of: decl, with: declDoc)
+            break
+        case OperatorDecl.self:
+            let id = OperatorDecl.ID(cursor)!
+            let decl = ctx.typedProgram.ast[id]!
+            
+            // Generate page
+            let declDoc = ctx.documentation.symbols.operatorDocs[astNodeId: id]!
+            GenerateOperatorPage(ctx: ctx, of: decl, with: declDoc)
+            break
+        case FunctionDecl.self:
+            let id = FunctionDecl.ID(cursor)!
+            let decl = ctx.typedProgram.ast[id]!
+            
+            // Generate page
+            let declDoc = ctx.documentation.symbols.functionDocs[astNodeId: id]!
+            GenerateFunctionPage(ctx: ctx, of: decl, with: declDoc)
+            break
+        case MethodDecl.self:
+            let id = MethodDecl.ID(cursor)!
+            let decl = ctx.typedProgram.ast[id]!
+            
+            // Add children on top of stack
+            for child in decl.impls {
+                stack.append(AnyDeclID(child))
+            }
+            
+            // Generate page
+            let declDoc = ctx.documentation.symbols.methodDeclDocs[astNodeId: id]!
+            GenerateMethodPage(ctx: ctx, of: decl, with: declDoc)
+            break
+        case MethodImpl.self:
+            let id = MethodImpl.ID(cursor)!
+            let decl = ctx.typedProgram.ast[id]!
+            
+            // Generate page
+            let declDoc = ctx.documentation.symbols.methodImplDocs[astNodeId: id]!
+            GenerateMethodImplementationPage(ctx: ctx, of: decl, with: declDoc)
+            break
+        case SubscriptDecl.self:
+            let id = SubscriptDecl.ID(cursor)!
+            let decl = ctx.typedProgram.ast[id]!
+            
+            // Add children on top of stack
+            for child in decl.impls {
+                stack.append(AnyDeclID(child))
+            }
+            
+            // Generate page
+            let declDoc = ctx.documentation.symbols.subscriptDeclDocs[astNodeId: id]!
+            GenerateSubscriptPage(ctx: ctx, of: decl, with: declDoc)
+            break
+        case SubscriptImpl.self:
+            let id = SubscriptImpl.ID(cursor)!
+            let decl = ctx.typedProgram.ast[id]!
+            
+            // Generate page
+            let declDoc = ctx.documentation.symbols.subscriptImplDocs[astNodeId: id]!
+            GenerateSubscriptImplementationPage(ctx: ctx, of: decl, with: declDoc)
+            break
+        case InitializerDecl.self:
+            let id = InitializerDecl.ID(cursor)!
+            let decl = ctx.typedProgram.ast[id]!
+            
+            // Generate page
+            let declDoc = ctx.documentation.symbols.initializerDocs[astNodeId: id]!
+            GenerateInitializerPage(ctx: ctx, of: decl, with: declDoc)
+            break
+        case TraitDecl.self:
+            let id = TraitDecl.ID(cursor)!
+            let decl = ctx.typedProgram.ast[id]!
+            
+            // Add children on top of stack
+            for child in decl.members {
+                stack.append(child)
+            }
+            
+            // Generate page
+            let declDoc = ctx.documentation.symbols.traitDocs[astNodeId: id]!
+            GenerateTraitPage(ctx: ctx, of: decl, with: declDoc)
+            break
+        case ProductTypeDecl.self:
+            let id = ProductTypeDecl.ID(cursor)!
+            let decl = ctx.typedProgram.ast[id]!
+            
+            // Add children on top of stack
+            for child in decl.members {
+                stack.append(child)
+            }
+            
+            // Generate page
+            let declDoc = ctx.documentation.symbols.productTypeDocs[astNodeId: id]!
+            GenerateProductTypePage(ctx: ctx, of: decl, with: declDoc)
+            break
+        default:
+            break
+        }
+    }
 }
