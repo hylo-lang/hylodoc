dbda8dde6970a69b16e35ab5fa58ed71ca63c9c9
E-vyatar <evyatarhadasi@gmail.com>
2024-05-20 11:15:19 +0200

    Start working with Stencil
    - Modify index.html with placeholders blocks for content
    - Write initial version of methods to generate module page
    - Write test without assertion for module page
    
    More dynamic approach
    
    - useValues is now renderPage
    - renderPage dynamiclly creates extending template, only with variables with given values (i.e. arr contains a key matching this variable) to prevent rendering error
    
    Module assst render
    
    Module asset render is done but still needs tests. Will make tests in future commit
    
    Update: merge changes from main
    
    Fix: able to compile
    
    access summary, incomplete
    
    - Adjust `AnyMarkdownNodeID` to prevent compilation error
    - Get summary from database
    
    Revert index.html in web-design to original content
    
    renderPage takes [key: (value, block string)] dictionary instead of [key: value]
    
    Trynig to make test work
    
    Start working with Stencil
    
    - Modify index.html with placeholders blocks for content
    - Write initial version of methods to generate module page
    - Write test without assertion for module page
    
    More dynamic approach
    
    - useValues is now renderPage
    - renderPage dynamiclly creates extending template, only with variables with given values (i.e. arr contains a key matching this variable) to prevent rendering error
    
    Module assst render
    
    Module asset render is done but still needs tests. Will make tests in future commit
    
    Update: merge changes from main
    
    Fix: able to compile
    
    access summary, incomplete
    
    - Adjust `AnyMarkdownNodeID` to prevent compilation error
    - Get summary from database
    
    Revert index.html in web-design to original content
    
    renderPage takes [key: (value, block string)] dictionary instead of [key: value]
    
    Trynig to make test wotk

--- Sources/WebsiteGen/Render/RenderAssets.swift
+++ Sources/WebsiteGen/Render/RenderAssets.swift
@@ -1,5 +1,6 @@
 import DocumentationDB
 import Foundation
+import Stencil
 
 /// Render the source-file page
 ///
@@ -24,8 +25,8 @@ public func renderArticlePage(ctx: GenerationContext, of: ArticleAsset) -> Strin
 }
 
 /// Renders the page for a folder
-/// 
-/// Displays the folder title as a title, optionally the content of the related article (FolderAsset.documentation), 
+///
+/// Displays the folder title as a title, optionally the content of the related article (FolderAsset.documentation),
 /// and a list of child assets along with links and their summaries.
 ///
 /// - Parameters:
@@ -34,7 +35,83 @@ public func renderArticlePage(ctx: GenerationContext, of: ArticleAsset) -> Strin
 ///
 /// - Returns: the contents of the rendered page
 public func renderFolderPage(ctx: GenerationContext, of: FolderAsset) -> String {
-    return ""
+    // get values
+    let title = of.name
+    let name = of.name
+    let summary = getSummary(
+        ctx.documentation.assetStore.articles[of.documentation!]!.content,
+        ctx.documentation.markdownStore
+        )
+    let children = of.children.map { getAssetTitle($0, ctx.documentation.assetStore) }
+
+    // create dictionary with structure [key: (value, block string)], for the renderPage function
+    let arr: [String: (Any, String)] = [
+        "title": (title, "{% block title %}{{ title }}{% endblock %}"),
+        "name": (name, "{% block name %}{{ name }}{% endblock %}"),
+        "summary": (summary, "{% block summary %}{{ summary }}{% endblock %}"),
+        "topics": (children, "{% block topics %}{% for topic in topics %}<p>{{ topic }}</p>{% endfor %}{% endblock %}")
+        ]
+
+    return renderPage(ctx.stencil, arr)
+}
+
+/// Insert values to the template page and render it
+/// - Parameters:
+///   - stencil: Stencil Environment
+///   - arr: [key: (value, block string)] dictionary with data for the temolate
+/// - Returns: HTML String with inserted values or throws an error from renderTemplate
+public func renderPage(_ stencil: Environment, _ arr: [String: (Any, String)]) -> String {
+    var res = "{% extends \"index.html\" %}"
+    // add blocks to the template
+    for el in arr {
+        res += el.value.1
+    }
+
+    // create dictionary with only values
+    let arrOnlyValues = arr.mapValues{ $0.0}
+
+    do {
+        return try stencil.renderTemplate(string: res, context: arrOnlyValues)
+    }
+    catch {
+        print("there was an error: \(error)")
+        return arr.description
+        // return res
+    }
+}
+
+/// Get the title of an asset
+/// - Parameters:
+///   - id: id of the asset
+///   - assetsDB: database with assets
+/// - Returns: String representing the title of the asset
+public func getAssetTitle(_ id: AnyAssetID, _ assetsDB: AssetStore) -> String {
+    switch id {
+    // case .sourceFile(let sid):
+        // return assetsDB.sourceFiles[documentationId: sid]!.name // TODO: Fix this
+    case .sourceFile(_) :
+        return "Source File title placeholder"
+    case .article(let aid):
+        return assetsDB.articles[aid]!.title!
+    case .module(let mid):
+        return assetsDB.modules[documentationId: mid]!.name
+    case .otherFile(_):
+        return "Other File"
+    }
+}
+
+/// Get the summary of a markdown node
+/// - Parameters:
+///   - id: id of the markdown node
+///   - st: database with markdown nodes
+/// - Returns: String representing the summary of the markdown node
+public func getSummary(_ id: AnyMarkdownNodeID, _ st: MarkdownStore) -> String {
+    switch id {
+    case .ofText(let tid):
+        return st.texts[tid]!.text
+    default:
+        return "Summary placeholder"
+    }
 }
 
 /// Render the other-file page and return the result
