9aa0b764e40d04c71c6eea77e31b1d74120af24e
Thomas van Weert <t.c.o.vanweert@student.tudelft.nl>
2024-06-01 02:23:44 +0200

    Initial implementation of new design

--- web-design/tailwind.config.js
+++ web-design/tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ['./src/js/**/*.{js,ts}', './**/*.html'],
   theme: {
+    fontFamily: {
+      inter: ['Inter'],
+      mono: ['Jetbrains Mono', 'monospace'],
+    },
     extend: {},
   },
   plugins: [],
