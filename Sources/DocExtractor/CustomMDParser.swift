3070de9c5c1f0e7a4a14a099a5bcc4217acedd58
Ambrus Toth <ping@ambrus.dev>
2024-06-04 10:24:32 +0200

    Custom MD Parser that parses double backticks as `HyloReference` custom text fragments.

---
+++ Sources/DocExtractor/CustomMDParser.swift
@@ -0,0 +1,140 @@
+import Foundation
+import MarkdownKit
+
+public struct HyloReference: CustomTextFragment, Equatable {
+  let text: String
+
+  public init(_ text: String) {
+    self.text = text
+  }
+
+  public func equals(to other: CustomTextFragment) -> Bool {
+    guard let that = other as? HyloReference else { return false }
+    return self == that
+  }
+
+  public func transform(via transformer: InlineTransformer) -> TextFragment {
+    return .custom(self)  // we don't allow transforming this fragment
+  }
+
+  public func generateHtml(via htmlGen: HtmlGenerator) -> String {
+    return "<code class=\"hylo-ref\">" + text + "</code>"
+  }
+
+  public var rawDescription: String {
+    return text
+  }
+  public var description: String {
+    return "hyloReference(\(text))"
+  }
+  public var debugDescription: String {
+    return "hyloReference(\(text))"
+  }
+}
+
+///
+/// An inline transformer which extracts code spans, auto-links and html tags and transforms
+/// them into `code`, `autolinks`, and `html` text fragments.
+/// This transformer is a modification of the `CodeLinkHtmlTransformer` from the `MarkdownKit` library.
+open class CodeRefLinkHtmlTransformer: InlineTransformer {
+
+  public override func transform(_ text: Text) -> Text {
+    var res: Text = Text()
+    var iterator = text.makeIterator()
+    var element = iterator.next()
+    loop: while let fragment = element {
+      switch fragment {
+      case .delimiter("`", let n, []):
+        var scanner = iterator
+        var next = scanner.next()
+        var count = 0
+        while let lookahead = next {
+          count += 1
+          switch lookahead {
+          case .delimiter("`", n, _):
+            var scanner2 = iterator
+            var code = ""
+            for _ in 1 ..< count {
+              code += scanner2.next()?.rawDescription ?? ""
+            }
+            if n == 2 {
+              res.append(fragment: .custom(HyloReference(code)))
+            } else {
+              res.append(fragment: .code(Substring(code)))
+            }
+            iterator = scanner
+            element = iterator.next()
+            continue loop
+          case .delimiter(_, _, _), .text(_), .softLineBreak, .hardLineBreak:
+            next = scanner.next()
+          default:
+            res.append(fragment: fragment)
+            element = iterator.next()
+            continue loop
+          }
+        }
+        res.append(fragment: fragment)
+        element = iterator.next()
+      case .delimiter("<", let n, []):
+        var scanner = iterator
+        var next = scanner.next()
+        var count = 0
+        while let lookahead = next {
+          count += 1
+          switch lookahead {
+          case .delimiter(">", n, _):
+            var scanner2 = iterator
+            var content = ""
+            for _ in 1 ..< count {
+              content += scanner2.next()?.rawDescription ?? ""
+            }
+            if isURI(content) {
+              res.append(fragment: .autolink(.uri, Substring(content)))
+              iterator = scanner
+              element = iterator.next()
+              continue loop
+            } else if isEmailAddress(content) {
+              res.append(fragment: .autolink(.email, Substring(content)))
+              iterator = scanner
+              element = iterator.next()
+              continue loop
+            } else if isHtmlTag(content) {
+              res.append(fragment: .html(Substring(content)))
+              iterator = scanner
+              element = iterator.next()
+              continue loop
+            }
+            next = scanner.next()
+          case .delimiter(_, _, _), .text(_), .softLineBreak, .hardLineBreak:
+            next = scanner.next()
+          default:
+            res.append(fragment: fragment)
+            element = iterator.next()
+            continue loop
+          }
+        }
+        res.append(fragment: fragment)
+        element = iterator.next()
+      default:
+        element = self.transform(fragment, from: &iterator, into: &res)
+      }
+    }
+    return res
+  }
+}
+
+final class HyloDocMarkdownParser: MarkdownParser {
+  override public class var defaultInlineTransformers: [InlineTransformer.Type] {
+    return [
+      DelimiterTransformer.self,
+      CodeRefLinkHtmlTransformer.self,
+      LinkTransformer.self,
+      EmphasisTransformer.self,
+      EscapeTransformer.self,
+    ]
+  }
+  override public class var standard: HyloDocMarkdownParser {
+    return self.singleton
+  }
+  private static let singleton = HyloDocMarkdownParser()
+}
---
+++ Tests/DocExtractorTests/CustomMDParserTest.swift
@@ -0,0 +1,221 @@
+import XCTest
+
+@testable import DocExtractor
+@testable import MarkdownKit
+
+public final class CustomMDParserTest: XCTestCase {
+
+  static let delimiterParser = HyloDocMarkdownParser(
+    blockParsers: nil,
+    inlineTransformers: [
+      DelimiterTransformer.self
+    ])
+
+  func testDelimiterParserWorksForMultipleBackticks() {
+    XCTAssertEqual(
+      Self.delimiterParser.parse("`simple code`"),
+      .document([
+        .paragraph(
+          Text([
+            .delimiter("`", 1, []),
+            .text("simple code"),
+            .delimiter("`", 1, []),
+          ]))
+      ])
+    )
+
+    XCTAssertEqual(
+      Self.delimiterParser.parse("``simple code``"),
+      .document([
+        .paragraph(
+          Text([
+            .delimiter("`", 2, []),
+            .text("simple code"),
+            .delimiter("`", 2, []),
+          ]))
+      ])
+    )
+
+    XCTAssertEqual(
+      Self.delimiterParser.parse("``simple`` `code`"),
+      .document([
+        .paragraph(
+          Text([
+            .delimiter("`", 2, []),
+            .text("simple"),
+            .delimiter("`", 2, []),
+            .text(" "),
+            .delimiter("`", 1, []),
+            .text("code"),
+            .delimiter("`", 1, []),
+          ]))
+      ])
+    )
+
+    XCTAssertEqual(
+      Self.delimiterParser.parse("<simple>"),
+      .document([
+        .paragraph(
+          Text([
+            .delimiter("<", 1, []),
+            .text("simple"),
+            .delimiter(">", 1, []),
+          ]))
+      ])
+    )
+
+    XCTAssertEqual(
+      Self.delimiterParser.parse("<https://google.com>"),
+      .document([
+        .paragraph(
+          Text([
+            .delimiter("<", 1, []),
+            .text("https://google.com"),
+            .delimiter(">", 1, []),
+          ]))
+      ])
+    )
+  }
+
+  func testNormalInlineCodeWorks() {
+    XCTAssertEqual(
+      HyloDocMarkdownParser.standard.parse("`simple code`"),
+      .document([
+        .paragraph(
+          Text([
+            .code("simple code")
+          ]))
+      ]),
+      "Single backticks should be parsed as inline code"
+    )
+
+    XCTAssertEqual(
+      HyloDocMarkdownParser.standard.parse("```simple code```"),
+      .document([
+        .paragraph(
+          Text([
+            .code("simple code")
+          ]))
+      ]),
+      "2+ backticks should be parsed as inline code"
+    )
+
+    XCTAssertEqual(
+      HyloDocMarkdownParser.standard.parse("````simple code````"),
+      .document([
+        .paragraph(
+          Text([
+            .code("simple code")
+          ]))
+      ]),
+      "2+ backticks should be parsed as inline code"
+    )
+  }
+
+  func testHyloCodeReferences() {
+    XCTAssertEqual(
+      HyloDocMarkdownParser.standard.parse("``simple code``"),
+      .document([
+        .paragraph(
+          Text([
+            .custom(HyloReference("simple code"))
+          ]))
+      ]),
+      "Double backticks should be parsed as Hylo references"
+    )
+
+    XCTAssertEqual(
+      HyloDocMarkdownParser.standard.parse("`` `simple code` ``"),
+      .document([
+        .paragraph(
+          Text([
+            .custom(HyloReference(" `simple code` "))
+          ]))
+      ]),
+      "Double backticks should be parsed as Hylo references, and inner backticks should be part of the content."
+    )
+
+    XCTAssertEqual(
+      HyloDocMarkdownParser.standard.parse("``MyType.`type` ``"),
+      .document([
+        .paragraph(
+          Text([
+            .custom(HyloReference("MyType.`type` "))
+          ]))
+      ]),
+      "Double backticks should be parsed as Hylo references, and inner backticks should be part of the content."
+    )
+  }
+
+  func testDummyHTMLAndDescriptionGeneration() {
+    XCTAssertEqual(
+      HyloReference("MyType.Element").generateHtml(via: HtmlGenerator.standard),
+      "<code class=\"hylo-ref\">MyType.Element</code>"
+    )
+    XCTAssertEqual(
+      HtmlGenerator.standard.generate(
+        doc: HyloDocMarkdownParser.standard.parse("``MyType.Element``")
+      ).trimmingSuffix(while: { $0.isWhitespace }),
+      "<p><code class=\"hylo-ref\">MyType.Element</code></p>"
+    )
+
+    XCTAssertEqual(
+      HyloReference("MyType.Element").description,
+      "hyloReference(MyType.Element)"
+    )
+    XCTAssertEqual(
+      HyloReference("MyType.Element").debugDescription,
+      "hyloReference(MyType.Element)"
+    )
+    XCTAssertEqual(
+      HyloReference("MyType.Element").rawDescription,
+      "MyType.Element"
+    )
+  }
+
+  func testOriginalFunctionalityStillWorks() {
+    // url
+    XCTAssertEqual(
+      HyloDocMarkdownParser.standard.parse("<https://google.com>"),
+      .document([
+        .paragraph(Text([.autolink(.uri, "https://google.com")]))
+      ]),
+      "autolink url"
+    )
+
+    // tel
+    XCTAssertEqual(
+      HyloDocMarkdownParser.standard.parse("<tel:123456>"),
+      .document([
+        .paragraph(Text([.autolink(.uri, "tel:123456")]))
+      ]),
+      "autolink tel"
+    )
+
+    // email
+    XCTAssertEqual(
+      HyloDocMarkdownParser.standard.parse("<a@b.com>"),
+      .document([
+        .paragraph(Text([.autolink(.email, "a@b.com")]))
+      ]),
+      "autolink email"
+    )
+
+    // html tags
+    XCTAssertEqual(
+      HyloDocMarkdownParser.standard.parse("<img src=\"hello\">"),
+      .document([
+        .paragraph(Text([.html("img src=\"hello\"")]))
+      ]),
+      "html tag"
+    )
+  }
+
+}
+
+extension Text {
+  init(_ fragments: [TextFragment]) {
+    self.init()
+    fragments.forEach { self.append(fragment: $0) }
+  }
+}
